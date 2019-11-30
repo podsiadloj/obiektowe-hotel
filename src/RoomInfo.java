e0cc3f36f44a48a99dc771cbac30b01406076435
Jan Podsiadło <janpodsiadlo95@gmail.com>
2019-11-30 15:19:08 +0100

    Fix RoomInfo

--- src/RoomInfo.java
+++ src/RoomInfo.java
@@ -1,18 +1,12 @@
 //nazwa pokoju i ilość ludzi w nim zakwaterowanych
 public class RoomInfo {
-    private String name;
-    private Integer number;
+    public String name;
+    public Integer number;
+    public Comfort comfort;
 
     public RoomInfo(String name, Integer number, Comfort comfort){
         this.name = name;
         this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getNumber() {
-        return number;
+        this.comfort = comfort;
     }
 }
