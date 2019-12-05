5505207c8a88b8796e3d282c68511c4da3dc4bfc
Jan Podsiadło <janpodsiadlo95@gmail.com>
2019-12-05 10:57:55 +0100

    Add managing events and seasons to interface

---
+++ src/Event.java
@@ -0,0 +1,9 @@
+public class Event {
+    public Period period;
+    public double priceModifier;
+
+    public Event(Period period, double priceModifier) {
+        this.period = period;
+        this.priceModifier = priceModifier;
+    }
+}
