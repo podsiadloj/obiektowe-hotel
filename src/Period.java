ab921ec2c1b0738047c8e273503f644e4cd17b08
Jan Podsiadło <janpodsiadlo95@gmail.com>
2019-12-05 11:13:51 +0100

    Improve Period class

--- src/Period.java
+++ src/Period.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.List;
 
 //Okres rezerwacji od-do
 public class Period {
@@ -8,10 +9,17 @@ public class Period {
     public Period(Date start, Date end) {
         this.start = start;
         this.end = end;
+        if(getDays() < 1){
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getDays(){
-        return (int) Math.ceil( (double) (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24));
+        return (int) Math.floor( (double) (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+    }
+
+    public boolean isDayInPeriod(Date day){
+        return day.getTime() >= start.getTime() && day.getTime() <= end.getTime(); //good enough since we always record dates with day precision and the same timezone
     }
 
     public Date getStart() {
